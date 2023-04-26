1f484a241068f249756adc4293dcc49ac8ba50d7
keroti <peterkeroti55@gmail.com>
2023-04-26 18:48:23 +0300

    Unlock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Function to determine if all boxes can be opened """
+
+
+def canUnlockAll(boxes):
+    """ This function will take a list of lists and the content of a list will unlock other lists """
+    n = len(boxes)
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < n:
+                keys.append(box)
+    if len(keys) == n:
+        return True
+    return False
