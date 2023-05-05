88efb7285511fc7fd192cee748b8f5043fa5189f
keroti <peterkeroti55@gmail.com>
2023-05-05 03:20:14 +0300

    Minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Script for minimum operations """
+
+
+def minOperations(n):
+    """
+    Calculates the minimum operation to result in exactly n H characters
+    Args:
+    n (int): The desired number of H characters
+    Returns:
+        int: The fewest number of operations needed to result in exactly n H characters
+    """
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+
+    return n
