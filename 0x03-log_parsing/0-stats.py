6014c2938d5669a9947fe40306e43074fe6d7cc5
keroti <peterkeroti55@gmail.com>
2023-05-11 23:22:52 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-"""Log Parsing function"""
+'''Module for log parsing'''
+
 
 import sys
 
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
-status_count = {code: 0 for code in status_codes}
+status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
+               '403': 0, '404': 0, '405': 0, '500': 0}
 size = 0
-line_count = 0
+counter = 0
 
-for line in sys.stdin:
-    try:
-        parts = line.split()
-        code = parts[-2]
-        size = int(parts[-1])
-        if code in status_codes:
-            status_count[code] += 1
-        size += size
-        line_count += 1
-        if line_count % 10 == 0:
-            print("File size: {}".format(size))
-            for code in sorted(status_count.keys()):
-                if status_count[code] > 0:
-                    print("{}: {}".format(code, status_count[code]))
-    except Exception:
-        pass
+try:
+    for i in sys.stdin:
+        list = i.split(" ")
+        if len(list) > 4:
+            code = list[-2]
+            size = int(list[-1])
+            if code in status_code.keys():
+                status_code[code] += 1
+            size += size
+            counter += 1
 
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(size))
+            for key, value in sorted(status_code.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-def print_metrics(code, size):
-    """Print Metrics of Total file size & Status Code count."""
-    print("File size: {:d}".format(size))
-    for i in sorted(code.keys()):
-        if status_count[i] > 0:
-            print("{}: {:d}".format(i, status_count[i]))
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(size))
+    for key, value in sorted(status_code.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
