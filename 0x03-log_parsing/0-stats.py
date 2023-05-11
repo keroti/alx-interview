36435aaf3ab24968e44b18ca20d5f076d67837b2
keroti <peterkeroti55@gmail.com>
2023-05-11 23:25:16 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-'''Module for log parsing'''
+'''a script that reads stdin line by line and computes metrics'''
 
 
 import sys
 
-status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
-               '403': 0, '404': 0, '405': 0, '500': 0}
-size = 0
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
 counter = 0
 
 try:
-    for i in sys.stdin:
-        list = i.split(" ")
-        if len(list) > 4:
-            code = list[-2]
-            size = int(list[-1])
-            if code in status_code.keys():
-                status_code[code] += 1
-            size += size
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
             counter += 1
 
         if counter == 10:
             counter = 0
-            print('File size: {}'.format(size))
-            for key, value in sorted(status_code.items()):
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
 
@@ -31,7 +31,7 @@ except Exception as err:
     pass
 
 finally:
-    print('File size: {}'.format(size))
-    for key, value in sorted(status_code.items()):
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
