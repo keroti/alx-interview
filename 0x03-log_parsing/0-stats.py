287f91887980ed40bca790a83cc1d83cd56ee51d
keroti <peterkeroti55@gmail.com>
2023-05-11 14:32:14 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
-"""Log Parsing function"""
-
 import sys
 
 
-def print_metrics(code, size):
-    """Print Metrics of Total file size & Status Code count."""
-    print("File size: {:d}".format(size))
-    for i in sorted(code.keys()):
-        if code[i] != 0:
-            print("{}: {:d}".format(i, code[i]))
+def print_stats(file_size, status_codes):
+    """Print the metrics"""
+    print("File size: {}".format(file_size))
+    for status_code in sorted(status_codes.keys()):
+        if status_codes[status_code] > 0:
+            print("{}: {}".format(status_code, status_codes[status_code]))
 
 
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
-
-size = 0
-count = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            print_metrics(status_codes, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in status_codes:
-                status_codes[stlist[-2]] += 1
-        except:
-            pass
-    print_metrics(status_codes, size)
+def parse_line(line):
+    """Parse a log line and return the file size and status code"""
+    fields = line.split()
+    if len(fields) < 7:
+        return None, None
+    try:
+        file_size = int(fields[6])
+        status_code = int(fields[8])
+    except (ValueError, IndexError):
+        return None, None
+    return file_size, status_code
 
 
-except KeyboardInterrupt:
-    print_metrics(status_codes, size)
-    raise
+# Initialize variables
+file_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
+
+# Read lines from stdin
+for line in sys.stdin:
+    # Parse the line
+    file_size, status_code = parse_line(line)
+    if file_size is None or status_code is None:
+        continue
+
+    # Update metrics
+    file_size += file_size
+    status_codes[status_code] += 1
+    line_count += 1
+
+    # Print stats every 10 lines or when interrupted
+    if line_count % 10 == 0:
+        print_stats(file_size, status_codes)
+
+# Print final stats
+print_stats(file_size, status_codes)
