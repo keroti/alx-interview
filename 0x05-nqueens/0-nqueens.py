11f5767f682842728c1c01521f467b67299fb52d
keroti <peterkeroti55@gmail.com>
2023-05-26 04:45:33 +0300

    N queens solve

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,72 +1,119 @@
 #!/usr/bin/python3
 """
-N queens puzzle solution
+N queens problem
 """
 import sys
 
 
-def is_safe(board, row, col):
+def first_board(n):
     """
-    Check if it's safe to place a queen at board[row][col]
+    Initialize a 2 by 2 sized chessboard.
     """
-    # Check left side of the current row
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Check upper diagonal on the left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on the left side
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
 
 
-def solve_nqueens(board, col, solutions):
+def board_copy(board):
     """
-    Solve the N queens problem using backtracking
+    Return a copy of a chessboard.
     """
-    if col >= len(board):
-        solutions.append([[i, j] for i, row in enumerate(board) for j, val in enumerate(row) if val == 1])
-        return
-
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            solve_nqueens(board, col + 1, solutions)
-            board[i][col] = 0
+    if isinstance(board, list):
+        return list(map(board_copy, board))
+    return (board)
 
 
-def print_solutions(n):
+def solution(board):
     """
-    Print all possible solutions to the N queens problem
+    Return the list of lists representation of a solved chessboard
     """
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+    solution = []
+    for k in range(len(board)):
+        for m in range(len(board)):
+            if board[k][m] == "Q":
+                solution.append([k, m])
+                break
+    return (solution)
 
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solutions = []
-    solve_nqueens(board, 0, solutions)
 
-    for solution in solutions:
-        print(solution)
+def spots(board, row, col):
+    """
+    Remove spots queen can no longer pklay
+    """
+    # forward spots
+    for x in range(col + 1, len(board)):
+        board[row][x] = "x"
+    # backwards spots
+    for x in range(col - 1, -1, -1):
+        board[row][x] = "x"
+    # spots below
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    # spots above
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
+    # spots diagonally down to the right
+    x = col + 1
+    for r in range(row + 1, len(board)):
+        if x >= len(board):
+            break
+        board[r][x] = "x"
+        x += 1
+    # spots diagonally up to the left
+    x = col - 1
+    for r in range(row - 1, -1, -1):
+        if x < 0:
+            break
+        board[r][x]
+        x -= 1
+    # spots diagonally up to the right
+    x = col + 1
+    for r in range(row - 1, -1, -1):
+        if x >= len(board):
+            break
+        board[r][x] = "x"
+        x += 1
+    # spots diagonally down to the left
+    x = col - 1
+    for r in range(row + 1, len(board)):
+        if x < 0:
+            break
+        board[r][x] = "x"
+        x -= 1
+
+
+def solve(board, row, queens, solutions):
+    """
+    Solve an N-queens puzzle and return solution
+    """
+    if queens == len(board):
+        solutions.append(solution(board))
+        return (solutions)
+
+    for y in range(len(board)):
+        if board[row][y] == " ":
+            new_board = board_copy(board)
+            new_board[row][y] = "Q"
+            spots(new_board, row, y)
+            solutions = solve(new_board, row + 1,
+                              queens + 1, solutions)
+
+    return (solutions)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
+    if sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-    print_solutions(n)
+    board = first_board(int(sys.argv[1]))
+    solutions = solve(board, 0, 0, [])
+    for solve in solutions:
+        print(solve)
