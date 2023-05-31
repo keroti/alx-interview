ec7af3d9dbb1e79aca9f898b115954fd42e49cd1
keroti <peterkeroti55@gmail.com>
2023-05-31 10:37:56 +0300

    Star wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,29 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
-function getCharacters(movieId) {
-    const url = `https://swapi.dev/api/films/${movieId}/`;
-    request(url, (error, response, body) => {
-        if (error) {
-            console.error(error);
-        } else {
-            const film = JSON.parse(body);
-            const characterUrls = film.characters;
-            characterUrls.forEach((characterUrl) => {
-                request(characterUrl, (err, res, charBody) => {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        const character = JSON.parse(charBody);
-                        console.log(character.name);
-                    }
-                });
-            });
-        }
-    });
+function getCharacters (movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const film = JSON.parse(body);
+      const characterUrls = film.characters;
+      characterUrls.forEach((characterUrl) => {
+        request(characterUrl, (err, res, charBody) => {
+          if (err) {
+            console.error(err);
+          } else {
+            const character = JSON.parse(charBody);
+            console.log(character.name);
+          }
+        });
+      });
+    }
+  });
 }
 if (require.main === module) {
-    const movieId = process.argv[2];
-    getCharacters(movieId);
+  const movieId = process.argv[2];
+  getCharacters(movieId);
 }
