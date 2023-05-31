9f5ca4fc37fc0e8b0bef04bf19e11257fc34307d
keroti <peterkeroti55@gmail.com>
2023-05-31 10:43:35 +0300

    Star wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,22 +2,28 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-request(url, async (error, response, body) => {
-  error && console.log(error);
-
-  const charactersArray = (JSON.parse(response.body).characters);
-  for (const character of charactersArray) {
-    await new Promise((resolve, reject) => {
-      request(character, (error, response, body) => {
-        error && console.log(error);
-
-        console.log(JSON.parse(body).name);
-        resolve();
+function getCharacters (movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const film = JSON.parse(body);
+      const characterUrls = film.characters;
+      characterUrls.forEach((characterUrl) => {
+        request(characterUrl, (err, res, charBody) => {
+          if (err) {
+            console.error(err);
+          } else {
+            const character = JSON.parse(charBody);
+            console.log(character.name);
+          }
+        });
       });
-    });
-  }
-});
+    }
+  });
+}
+if (require.main === module) {
+  const movieId = process.argv[2];
+  getCharacters(movieId);
+}
