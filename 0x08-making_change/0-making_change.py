3b55dbb9fcf2a4abc217a137c91cb5c93a7424f6
Peter Keroti <59761453+keroti@users.noreply.github.com>
2023-08-25 00:04:41 +0300

    Making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Making Change function"""
+
+
+def makeChange(coins, total):
+    """This function will determine the fewest
+    number of coins needed to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for j in coin:
+            while(total >= j):
+                counter += 1
+                total -= j
+        if total == 0:
+            return counter
+        return -1
