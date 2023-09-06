2c6ec04d0e72a13d3592e14ab1dfd8bd9ddeb712
keroti <peterkeroti55@gmail.com>
2023-09-06 20:57:06 +0300

    Primes game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,39 +1,33 @@
 #!/usr/bin/python3
 """
-Module: Game of choosing Prime numbers
+Module for prime game
 """
 
 
 def primeNumbers(n):
-    """Return list of prime numbers between 1 and n inclusive
-       Args:
-        n (int): upper boundary of range. lower boundary is always 1
     """
-    primeNos = []
+    Function to return a list of prime numbers
+    """
+    primeNums = []
     filtered = [True] * (n + 1)
     for prime in range(2, n + 1):
         if (filtered[prime]):
-            primeNos.append(prime)
+            primeNums.append(prime)
             for i in range(prime, n + 1, prime):
                 filtered[i] = False
-    return primeNos
+    return primeNums
 
 
 def isWinner(x, nums):
     """
-    Determines winner of Prime Game
-    Args:
-        x (int): no. of rounds of game
-        nums (int): upper limit of range for each round
-    Return:
-        Name of winner (Maria or Ben) or None if winner cannot be found
+    Function to determine who the winner of the game is
     """
     if x is None or nums is None or x == 0 or nums == []:
         return None
     Maria = Ben = 0
     for i in range(x):
-        primeNos = primeNumbers(nums[i])
-        if len(primeNos) % 2 == 0:
+        primeNums = primeNumbers(nums[i])
+        if len(primeNums) % 2 == 0:
             Ben += 1
         else:
             Maria += 1
